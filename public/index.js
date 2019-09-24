34ff226c0be4e177c611a769b32c74eeb172648a
jo3l17 <47827494+jo3l17@users.noreply.github.com>
2019-09-24 17:45:53 -0500

    nose

--- public/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const googlePlaceDetails = require('./google-place-details');
-module.exports = function (app, db) {
-    googlePlaceDetails(app, db);
-    // Other route groups could go here, in the future
-};
